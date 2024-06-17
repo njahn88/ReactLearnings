e58f33db3ad731682261a82fb9e1a79ff2546eb5
njahn88 <nja010@email.latech.edu>
2024-06-16 20:10:12 -0500

    Added body elements using React based JavaScript

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -4,31 +4,78 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Reach allows for easy creation of reusable HTML using JavaScript
+
+const welcome = 'Welcome to the page'
+const title = 'Getting started with React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Nathan',
+  lastName: 'Jahn',
+}
+const date = 'June 16, 2024'
+
 
 const header = (
   <header>
-    <h1>Welcome to the page</h1>
-    <h2>Getting started with react</h2>
-    <h3>Javascript Library</h3>
-    <p>Nathan Jahn</p>
-    <small>June 16, 2024</small>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>
   </header>
 )
 
+const numOne = 3
+const numTwo = 2
+
+const result = (
+  <p>
+    {numOne} + {numTwo} = {numOne + numTwo}
+  </p>
+)
+
+// Creating variables can allow you to insert them using {}
+const yearBorn = 2000
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
+)
+
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+// Using className="?" allows you to pull from the header style if
+// desired
 const main = (
   <main>
-    <p>Prerequisite to getting started with reach.js:</p>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>Javascript</li>
-    </ul>
+    <div className='main-wrapper'>
+      <p>Prerequisite to getting started with {' '}
+        <strong>
+          <em>react.js</em>
+        </strong>
+      </p>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
+    </div>
   </main>
 )
 
+const copyRight = 'Copyright 2024'
 const footer = (
   <footer>
-    <p>Copyright 2024</p>
+    <div className='footer-wrapper'>
+      <p>{copyRight}</p>
+    </div>
   </footer>
 )
 
