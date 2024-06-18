ec492dab32b54af552ed2196e900e0498f9bdc1a
njahn88 <nja010@email.latech.edu>
2024-06-18 10:03:43 -0500

    Cleaned index.js

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -31,22 +31,72 @@ class Header extends React.Component {
   }
 }
 
-const AppMain = () => {
-  const data = {
-    welcome: 'Welcome to the page',
-    title: 'Getting started with React',
-    subtitle: 'JavaScript Library',
-    author: {
-      firstName: 'Nathan',
-      lastName: 'Jahn',
-    },
-    date: 'June 18, 2024'
+class TechList extends React.Component {
+  constructor(props){
+    super(props)
   }
-  return (
-    <div className='app'>
-      <Header data={data}/>
-    </div>
-  )
+  render(){
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+class Main extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisites to getting started with react.js</p>
+          <ul>
+            <TechList techs={this.props.techs}/>
+          </ul>
+        </div>
+      </main>
+    )
+  }
+}
+
+class Footer extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class AppMain extends React.Component {
+  render() {
+    const data = {
+      welcome: 'Welcome to the page',
+      title: 'Getting started with React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Nathan',
+        lastName: 'Jahn',
+      },
+      date: 'June 18, 2024',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+
+    return (
+      <div className='app'>
+        <Header data={data}/>
+        <Main techs={techs}/>
+        <Footer date={new Date()}/>
+      </div>
+    )
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
