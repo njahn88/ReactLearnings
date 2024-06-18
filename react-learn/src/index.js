5f1aacbcdbc6c4c9f1b9af3adf66a898a8f97680
njahn88 <nja010@email.latech.edu>
2024-06-18 09:44:06 -0500

    Deconstructing props to make them cleaner / easier to use within classes

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -10,14 +10,21 @@ class Header extends React.Component {
     super(props)
   }
   render() {
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
     return (
       <header>
         <div className='header-wrapper'>
-            <h1>{this.props.data.welcome}</h1>
-            <h2>{this.props.data.title}</h2>
-            <h3>{this.props.data.author.firstName}</h3>
-            <p>{this.props.data.author.lastName}</p>
-            <small>{this.props.data.date}</small>
+            <h1>{welcome}</h1>
+            <h2>{title}</h2>
+            <h3>{subtitle}</h3>
+            <p>{firstName} {lastName}</p>
+            <small>{date}</small>
         </div>
       </header>
     )
